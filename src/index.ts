dbd2ff9bb0aee805fc378d581cc57432338f99a3
Deathvenom54 <fdragon602@gmail.com>
2021-09-26 18:29:57 +0530

    ✨ Exported all needed things from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { initializeBot } from './core/initializeBot';
+export * from './structures/SlashCommand';
+export * from './structures/MarshalOptions';
+export * from './core/commands/loadCommandsFromDir';
+export * from './core/commands/loadCommands';
