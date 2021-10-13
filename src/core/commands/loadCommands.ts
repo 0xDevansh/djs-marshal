b4016b850103974c27ace75ab3cadfd4464662ce
Deathvenom54 <fdragon602@gmail.com>
2021-10-13 13:30:20 +0530

    (feat)[loadCommands] Warn when both defer and deferEphemeral are true

--- src/core/commands/loadCommands.ts
+++ src/core/commands/loadCommands.ts
@@ -1,6 +1,7 @@
 import { SlashCommand } from '../../structures/SlashCommand';
 import { Client, Collection, Snowflake } from 'discord.js';
 import { syncCommands } from './syncCommands';
+import { logWarning } from '../../misc/logWarning';
 
 /**
  * Load SlashCommands and store them as client.commands
@@ -12,6 +13,9 @@ export const loadCommands = (client: Client, commands: Array<SlashCommand>): voi
 
   commandsCollection.set('global', []);
   commands.forEach((command) => {
+    // pre-load checks
+    if (command.defer && command.deferEphemeral)
+      logWarning(`defer and deferEphemeral are both true for command ${command.name}`);
     // is guild command
     if ('guildId' in command && command.guildId) {
       if (commandsCollection.get(command.guildId)) commandsCollection.get(command.guildId)?.push(command);
