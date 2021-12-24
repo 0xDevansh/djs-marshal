d6f3fb4a13a696cfdd9d8e7c6f9603168551ffb0
DeathVenom54 <fdragon602@gmail.com>
2021-12-24 21:57:02 +0530

    [feat](SlashCommand) Make all members exported

--- src/structures/SlashCommand.ts
+++ src/structures/SlashCommand.ts
@@ -1,6 +1,6 @@
 import { ChatInputApplicationCommandData, CommandInteraction, PermissionString, Snowflake } from 'discord.js';
 
-type BaseSlashCommand = ChatInputApplicationCommandData & {
+export type BaseSlashCommand = ChatInputApplicationCommandData & {
   execute: (command: CommandInteraction) => void | Promise<void>;
   beforeExecute?: {
     defer?: boolean;
@@ -12,7 +12,7 @@ export type RegularSlashCommand = BaseSlashCommand & {
   commandType: 'global' | 'allGuild';
 };
 
-type GuildSlashCommand = BaseSlashCommand & {
+export type GuildSlashCommand = BaseSlashCommand & {
   commandType: 'guild';
   guildId: Snowflake;
   allowWithPermission?: PermissionString[];
