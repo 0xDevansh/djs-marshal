d13812b72045b924094da2522fedaf2e27275e22
DeathVenom54 <fdragon602@gmail.com>
2022-05-02 17:13:28 +0530

    [keep](...) Slight linting

--- src/core/definers/defineSlashCommand.ts
+++ src/core/definers/defineSlashCommand.ts
@@ -1,23 +1,25 @@
-import {SlashCommand} from "../../structures/SlashCommand";
-import chalk from "chalk";
+import { SlashCommand } from '../../structures/SlashCommand';
+import chalk from 'chalk';
 
 const logSimpleWarning = (warning: string) => {
-  console.log(chalk.yellow("warn ") + warning)
-}
+  console.log(chalk.yellow('warn ') + warning);
+};
 
 export const defineSlashCommand = (command: SlashCommand): SlashCommand => {
   if (!command.name) {
-    logSimpleWarning(`The required property name is missing in this SlashCommand, setting it to "unknown"`)
-    command.name = 'unknown'
+    logSimpleWarning(`The required property name is missing in this SlashCommand, setting it to "unknown"`);
+    command.name = 'unknown';
   }
   if (!command.description) {
-    logSimpleWarning(`The required property description is missing in SlashCommand ${command.name}, setting it to "No description"...`)
-    command.description = 'No description'
+    logSimpleWarning(
+      `The required property description is missing in SlashCommand ${command.name}, setting it to "No description"...`,
+    );
+    command.description = 'No description';
   }
   if (!command.execute) {
-    logSimpleWarning(`The execute callback is missing in SlashCommand ${command.name}`)
-    command.execute = (c) => console.error(`The slash command '${c.commandName}' has not been implemented yet`)
+    logSimpleWarning(`The execute callback is missing in SlashCommand ${command.name}`);
+    command.execute = (c) => console.error(`The slash command '${c.commandName}' has not been implemented yet`);
   }
 
   return command;
-}
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {initializeBot} from './core/initializeBot';
+import { initializeBot } from './core/initializeBot';
 
-export {initializeBot} from './core/initializeBot';
+export { initializeBot } from './core/initializeBot';
 
 export * from './structures/SlashCommand';
 export * from './structures/ButtonCommand';
@@ -10,23 +10,23 @@ export * from './structures/logging';
 
 export * from './core/commands/loadCommandsFromDir';
 export * from './core/commands/loadCommands';
-import {loadCommandsFromDir} from './core/commands/loadCommandsFromDir';
-import {loadCommands} from './core/commands/loadCommands';
+import { loadCommandsFromDir } from './core/commands/loadCommandsFromDir';
+import { loadCommands } from './core/commands/loadCommands';
 
 export * from './core/buttons/loadButtonsFromDir';
 export * from './core/buttons/loadButtons';
-import {loadButtonsFromDir} from './core/buttons/loadButtonsFromDir';
-import {loadButtons} from './core/buttons/loadButtons';
+import { loadButtonsFromDir } from './core/buttons/loadButtonsFromDir';
+import { loadButtons } from './core/buttons/loadButtons';
 
 export * from './core/selectMenus/loadSelectMenusFromDir';
 export * from './core/selectMenus/loadSelectMenus';
 
-import {loadSelectMenusFromDir} from './core/selectMenus/loadSelectMenusFromDir';
-import {loadSelectMenus} from './core/selectMenus/loadSelectMenus';
+import { loadSelectMenusFromDir } from './core/selectMenus/loadSelectMenusFromDir';
+import { loadSelectMenus } from './core/selectMenus/loadSelectMenus';
 
-import {handleInteraction} from './core/handlers/handleInteraction';
-import {handleGuildJoin} from './core/handlers/handleGuildJoin';
-import {handleGuildMemberUpdate} from './core/handlers/handleGuildMemberUpdate';
+import { handleInteraction } from './core/handlers/handleInteraction';
+import { handleGuildJoin } from './core/handlers/handleGuildJoin';
+import { handleGuildMemberUpdate } from './core/handlers/handleGuildMemberUpdate';
 
 export * from './core/handlers/handleInteraction';
 export * from './core/handlers/handleGuildJoin';
@@ -34,12 +34,12 @@ export * from './core/handlers/handleGuildMemberUpdate';
 
 export * from './core/commands/syncCommands';
 
-export * from './core/definers/defineSlashCommand'
-export * from './core/definers/defineButtonCommand'
-export * from './core/definers/defineSelectMenuCommand'
-import {defineSlashCommand} from "./core/definers/defineSlashCommand";
-import {defineButtonCommand} from "./core/definers/defineButtonCommand";
-import {defineSelectMenuCommand} from "./core/definers/defineSelectMenuCommand";
+export * from './core/definers/defineSlashCommand';
+export * from './core/definers/defineButtonCommand';
+export * from './core/definers/defineSelectMenuCommand';
+import { defineSlashCommand } from './core/definers/defineSlashCommand';
+import { defineButtonCommand } from './core/definers/defineButtonCommand';
+import { defineSelectMenuCommand } from './core/definers/defineSelectMenuCommand';
 
 const handlers = {
   handleInteraction,
@@ -61,5 +61,5 @@ export default {
 
   defineSlashCommand,
   defineButtonCommand,
-  defineSelectMenuCommand
+  defineSelectMenuCommand,
 };
